31aedea7229e5de1e7fca376a6e9f0e11ee66915
Konstantin Mikheev <mik.kon6610@gmail.com>
2026-06-25 14:56:18 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -45,14 +45,8 @@ export const App = () => {
               Чемпион в своём городе
             </Typography.Text>
           </div>
-          <img
-            src={hbImg}
-            alt="HB"
-            width="100%"
-            height={260}
-            style={{ objectFit: 'cover', transform: 'scale(1.1)', margin: '-1rem 0 1rem' }}
-          />
-          <Typography.Text view="primary-small" color="secondary">
+          <img src={hbImg} alt="HB" width="100%" height={260} style={{ objectFit: 'contain' }} />
+          <Typography.Text view="primary-small" color="secondary" style={{ maxWidth: '335px' }}>
             Стань чемпионом своего города в рейтинговых играх
           </Typography.Text>
         </div>
